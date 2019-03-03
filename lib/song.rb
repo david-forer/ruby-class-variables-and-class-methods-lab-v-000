a48f10480bc2d8cc28bb8ce63423e9d1d0e4bec0
David Forer <david.j.forer@gmail.com>
2019-03-03 14:20:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
     @artist = artist
     @@genre = genre
     @@count += 1 
-    @@artists << artists
-    @@genre << genre 
+    @@artists << artist
+    @@genres << genre 
   end
   
   def self.count
